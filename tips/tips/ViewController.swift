4cc0f4b3cca6dd76306ee431c355473bdddb0d65
Yashwita Mishra <ymishra@WDHL1488aeed2.local>
2017-03-17 09:51:56 -0700

    Tip calculator- implement optional requirements to fetch locale/currency from settings
    Tip calculator- implement optional requirements to fetch
    locale/currency from settings

--- tips/tips/ViewController.swift
+++ tips/tips/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     
     
     let defaults = UserDefaults.standard
+    let currencySymbol = NSLocale.current.currencySymbol
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -66,9 +67,22 @@ class ViewController: UIViewController {
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
         
+        let tipTotal = tip as NSNumber
+        let finalTotal = total as NSNumber
+
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = NumberFormatter.Style.currency
+        // localize to your grouping and decimal separator
         
-        tipLabel.text=String(format: "$%.2f", tip)
-        totalLabel.text=String(format: "$%.2f", total)
+        currencyFormatter.locale = NSLocale.current
+        let tipTotalString = currencyFormatter.string(from: tipTotal)
+        let finalTotalString = currencyFormatter.string(from: finalTotal)
+        
+        tipLabel.text=tipTotalString
+        totalLabel.text=finalTotalString
+        
+        
     }
 
     @IBAction func onPercentageSwitch(_ sender: Any) {
